d0f3d805a93d071a5d18c0f7d236cb6bf128b362
Vidisha Mistry <vs221@ic.ac.uk>
2025-02-10 06:30:37 +0000

    16 tests* + clean up

--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -2,14 +2,14 @@ import { Entry } from "@prisma/client";
 import Prisma from "../src/db";
 import { server } from "../src/server";
 
-jest.mock('../src/db', () => ({
+jest.mock("../src/db", () => ({
   entry: {
     findMany: jest.fn(),
     findUnique: jest.fn(),
     create: jest.fn(),
     update: jest.fn(),
     delete: jest.fn(),
-  }
+  },
 }));
 
 describe("server test", () => {
@@ -23,16 +23,16 @@ const mockCard: Entry = {
   title: "test",
   description: "description",
   created_at: new Date(),
-  due_for: new Date()
-}
+  due_for: new Date(),
+};
 
 const noDueDateMockCard: Entry = {
   id: "456",
   title: "test",
   description: "description",
   created_at: new Date(),
-  due_for: null
-}
+  due_for: null,
+};
 
 describe("Test: Create", () => {
   it("should create a sucessful card", async () => {
@@ -41,12 +41,12 @@ describe("Test: Create", () => {
     const resp = await server.inject({
       method: "POST",
       url: "/create/",
-      payload: mockCard
-    })
+      payload: mockCard,
+    });
 
     const body: Entry = JSON.parse(resp.body);
-    body.created_at = new Date(body.created_at)
-    body.due_for = body.due_for ? new Date(body.due_for) : null
+    body.created_at = new Date(body.created_at);
+    body.due_for = body.due_for ? new Date(body.due_for) : null;
     expect(resp.statusCode).toBe(200);
     expect(body).toEqual(mockCard);
   });
@@ -57,12 +57,12 @@ describe("Test: Create", () => {
     const resp = await server.inject({
       method: "POST",
       url: "/create/",
-      payload: noDueDateMockCard
-    })
+      payload: noDueDateMockCard,
+    });
 
     const body: Entry = JSON.parse(resp.body);
-    body.created_at = new Date(body.created_at)
-    body.due_for = body.due_for ? new Date(body.due_for) : null
+    body.created_at = new Date(body.created_at);
+    body.due_for = body.due_for ? new Date(body.due_for) : null;
     expect(resp.statusCode).toBe(200);
     expect(body).toEqual(noDueDateMockCard);
   });
@@ -76,9 +76,9 @@ describe("Test: Retrieve", () => {
     const response = await server.inject({
       method: "GET",
       url: "/get/",
-    })
+    });
 
-    const tempBodies: Entry[] = JSON.parse(response.body)
+    const tempBodies: Entry[] = JSON.parse(response.body);
 
     const bodies: Entry[] = tempBodies.map((body) => ({
       ...body,
@@ -94,16 +94,16 @@ describe("Test: Retrieve", () => {
 
     const response = await server.inject({
       method: "GET",
-      url:`/get/${mockCard.id}`,
-    })
+      url: `/get/${mockCard.id}`,
+    });
 
     const body: Entry = JSON.parse(response.body);
-    body.created_at = new Date(body.created_at)
-    body.due_for = body.due_for ? new Date(body.due_for) : null
+    body.created_at = new Date(body.created_at);
+    body.due_for = body.due_for ? new Date(body.due_for) : null;
 
     expect(response.statusCode).toBe(200);
     expect(body).toEqual(mockCard);
-  })
+  });
 
   it("should return empty array if there are no cards in database", async () => {
     // used same code as return all cards for certainity
@@ -112,9 +112,9 @@ describe("Test: Retrieve", () => {
     const response = await server.inject({
       method: "GET",
       url: "/get/",
-    })
+    });
 
-    const tempBodies: Entry[] = JSON.parse(response.body)
+    const tempBodies: Entry[] = JSON.parse(response.body);
 
     const bodies: Entry[] = tempBodies.map((body) => ({
       ...body,
@@ -123,159 +123,153 @@ describe("Test: Retrieve", () => {
     }));
 
     expect(bodies).toEqual([]);
-  })
+  });
 });
 
 describe("Test: Update (general)", () => {
   it("should update the title of the card", async () => {
-
-    const updatedCard: Entry = {...mockCard, title: "new title"};
-
-    const request = await server.inject({
-      method: "PUT",
-      url: `/update/${mockCard.id}`,
-      payload: updatedCard
-    })
-
-    updatedCard.created_at = new Date(updatedCard.created_at)
-    updatedCard.due_for = updatedCard.due_for ? new Date(updatedCard.due_for) : null
-
-    expect(request.statusCode).toEqual(200);
-    expect(Prisma.entry.update).toHaveBeenCalledWith({
-      where: { id: mockCard.id }, // to ensure the original card it being updated
-      data: updatedCard,
-    })
-  })
-
-  it("should update the description of the card", async () => {
-
-    const updatedCard: Entry = {...mockCard, description: "new description"};
-    
+    const updatedCard: Entry = { ...mockCard, title: "new title" };
 
     const request = await server.inject({
       method: "PUT",
       url: `/update/${mockCard.id}`,
-      payload: updatedCard
-    })
-    
-    updatedCard.created_at = new Date(updatedCard.created_at)
-    updatedCard.due_for = updatedCard.due_for ? new Date(updatedCard.due_for) : null
+      payload: updatedCard,
+    });
+
+    updatedCard.created_at = new Date(updatedCard.created_at);
+    updatedCard.due_for = updatedCard.due_for ? new Date(updatedCard.due_for) : null;
 
     expect(request.statusCode).toEqual(200);
     expect(Prisma.entry.update).toHaveBeenCalledWith({
       where: { id: mockCard.id }, // to ensure the original card it being updated
       data: updatedCard,
     });
-  })
+  });
+
+  it("should update the description of the card", async () => {
+    const updatedCard: Entry = { ...mockCard, description: "new description" };
+
+    const request = await server.inject({
+      method: "PUT",
+      url: `/update/${mockCard.id}`,
+      payload: updatedCard,
+    });
+
+    updatedCard.created_at = new Date(updatedCard.created_at);
+    updatedCard.due_for = updatedCard.due_for ? new Date(updatedCard.due_for) : null;
+
+    expect(request.statusCode).toEqual(200);
+    expect(Prisma.entry.update).toHaveBeenCalledWith({
+      where: { id: mockCard.id }, // to ensure the original card it being updated
+      data: updatedCard,
+    });
+  });
 
   it("should update the created_at date of the card", async () => {
     var yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
 
-    const updatedCard: Entry = {...mockCard, created_at:yesterday};
+    const updatedCard: Entry = { ...mockCard, created_at: yesterday };
 
     const request = await server.inject({
       method: "PUT",
       url: `/update/${mockCard.id}`,
-      payload: updatedCard
-    })
+      payload: updatedCard,
+    });
 
-    const body: Entry = {...updatedCard};
-    body.created_at = new Date(body.created_at)
-    body.due_for = body.due_for ? new Date(body.due_for) : null
+    const body: Entry = { ...updatedCard };
+    body.created_at = new Date(body.created_at);
+    body.due_for = body.due_for ? new Date(body.due_for) : null;
 
     expect(request.statusCode).toEqual(200);
     expect(Prisma.entry.update).toHaveBeenCalledWith({
       where: { id: mockCard.id }, // to ensure the original card it being updated
       data: body,
     });
-  })
-
-
-})
+  });
+});
 
 describe("Test: Update (due date)", () => {
   it("should update the due date of the card", async () => {
     var nextDay = new Date();
     nextDay.setDate(nextDay.getDate() + 1);
 
-    const updatedCard: Entry = {...mockCard, due_for:nextDay};
+    const updatedCard: Entry = { ...mockCard, due_for: nextDay };
 
     const request = await server.inject({
       method: "PUT",
       url: `/update/${mockCard.id}`,
-      payload: updatedCard
-    })
+      payload: updatedCard,
+    });
 
-    const body: Entry = {...updatedCard};
-    body.created_at = new Date(body.created_at)
-    body.due_for = body.due_for ? new Date(body.due_for) : null
+    const body: Entry = { ...updatedCard };
+    body.created_at = new Date(body.created_at);
+    body.due_for = body.due_for ? new Date(body.due_for) : null;
 
     expect(request.statusCode).toEqual(200);
     expect(Prisma.entry.update).toHaveBeenCalledWith({
       where: { id: mockCard.id }, // to ensure the original card it being updated
       data: body,
     });
-  })
+  });
 
   it("should update the due date with null", async () => {
-
-    const updatedCard: Entry = {...mockCard, due_for:null};
+    const updatedCard: Entry = { ...mockCard, due_for: null };
 
     const request = await server.inject({
       method: "PUT",
       url: `/update/${mockCard.id}`,
-      payload: updatedCard
-    })
+      payload: updatedCard,
+    });
 
-    const body: Entry = {...updatedCard};
-    body.created_at = new Date(body.created_at)
-    body.due_for = body.due_for ? new Date(body.due_for) : null
+    const body: Entry = { ...updatedCard };
+    body.created_at = new Date(body.created_at);
+    body.due_for = body.due_for ? new Date(body.due_for) : null;
 
     expect(request.statusCode).toEqual(200);
     expect(Prisma.entry.update).toHaveBeenCalledWith({
       where: { id: mockCard.id }, // to ensure the original card it being updated
       data: body,
     });
-  })
+  });
 
   it("should update null with due date", async () => {
     var nextDay = new Date();
     nextDay.setDate(nextDay.getDate() + 1);
 
-    const updatedCard: Entry = {...noDueDateMockCard, due_for:nextDay};
+    const updatedCard: Entry = { ...noDueDateMockCard, due_for: nextDay };
 
     const request = await server.inject({
       method: "PUT",
       url: `/update/${noDueDateMockCard.id}`,
-      payload: updatedCard
-    })
+      payload: updatedCard,
+    });
 
-    const body: Entry = {...updatedCard};
-    body.created_at = new Date(body.created_at)
-    body.due_for = body.due_for ? new Date(body.due_for) : null
+    const body: Entry = { ...updatedCard };
+    body.created_at = new Date(body.created_at);
+    body.due_for = body.due_for ? new Date(body.due_for) : null;
 
     expect(request.statusCode).toEqual(200);
     expect(Prisma.entry.update).toHaveBeenCalledWith({
       where: { id: noDueDateMockCard.id }, // to ensure the original card it being updated
       data: body,
     });
-  })
-})
+  });
+});
 
 describe("Test: Delete", () => {
   it("should delete a specific card from db", async () => {
     const request = await server.inject({
       method: "DELETE",
-      url: `/delete/${mockCard.id}`
-    })
+      url: `/delete/${mockCard.id}`,
+    });
 
     expect(request.statusCode).toEqual(200);
     expect(Prisma.entry.delete).toHaveBeenCalledWith({
       where: { id: mockCard.id },
     });
-  })
-})
+  });
+});
 
 describe("Test: Error handling", () => {
   const invalidID = "999";
@@ -286,51 +280,49 @@ describe("Test: Error handling", () => {
     const response = await server.inject({
       method: "POST",
       url: "/create/",
-      payload: mockCard
-    })
+      payload: mockCard,
+    });
 
     expect(response.statusCode).toBe(500);
-    expect(response.body.toString()).toContain("Error creating entry")
-  })
+    expect(response.body.toString()).toContain("Error creating entry");
+  });
 
   it("should return the correct error message when retrival was unsucessful", async () => {
     (Prisma.entry.findUnique as jest.Mock).mockResolvedValue(null);
 
     const response = await server.inject({
       method: "GET",
-      url:`/get/${invalidID}`,
-    })
+      url: `/get/${invalidID}`,
+    });
 
     expect(response.statusCode).toBe(500);
     expect(response.body).toContain(`Error finding entry with id ${invalidID}`);
-  })
+  });
 
   it("should return the correct error message when update was unsucessful", async () => {
     const updatedCard: Entry = { ...mockCard, description: "new description" };
 
     (Prisma.entry.update as jest.Mock).mockRejectedValueOnce(new Error("error"));
-    
+
     const response = await server.inject({
       method: "PUT",
       url: `/update/${mockCard.id}`,
-      payload: updatedCard
+      payload: updatedCard,
     });
-    
+
     expect(response.statusCode).toBe(500);
     expect(response.body).toContain("Error updating");
-  })
+  });
 
   it("should return the correct error message when delete was unsucessful", async () => {
-    
     (Prisma.entry.delete as jest.Mock).mockRejectedValueOnce(new Error("error"));
-    
+
     const response = await server.inject({
       method: "DELETE",
       url: `/delete/${invalidID}`,
     });
-    
+
     expect(response.statusCode).toBe(500);
     expect(response.body).toContain("Error deleting entry");
-  })
-
-})
+  });
+});
